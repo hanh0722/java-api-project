4f1f1e32cfd5068031e1cadd16e8260394c64a9f
Brian07 <hoanganh0722@hanh0722.local>
2021-11-19 10:20:36 +0700

    fix blog model

--- src/main/java/com/example/demo/controller/Blog/BlogController.java
+++ src/main/java/com/example/demo/controller/Blog/BlogController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.example.demo.model.Blog.Blog;
 import com.example.demo.service.Blog.BlogService;
+import com.example.demo.util.ErrrorException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,19 +32,31 @@ public class BlogController {
     }
 
     @PostMapping
-    public ResponseEntity<Blog> createBlog(@RequestBody Blog blog) {
-        Blog newBlog = blogService.createBlog(blog);
+    public ResponseEntity<?> createBlog(@RequestBody Blog blog) {
+        try{
+            Blog newBlog = blogService.createBlog(blog);
         return ResponseEntity.ok(newBlog);
+        }catch(Exception err){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrrorException(500, "Internal Server Error"));
+        }
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Blog> findBlogById(@PathVariable String id) {
-        Blog blog = blogService.findBlogById(id);
-        return ResponseEntity.ok(blog);
+    public ResponseEntity<?> findBlogById(@PathVariable String id) {
+        try{
+            Blog blog = blogService.findBlogById(id);
+            return ResponseEntity.ok(blog);
+        }catch(Exception err){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrrorException(404, "Post is not existed"));
+        }
     }
     @PutMapping("/{id}")
-    public ResponseEntity<Blog> changeBlogById(@PathVariable String id, @RequestBody Blog blog) {
-        Blog newBlogAfterUpdate = blogService.changeBlogById(id, blog);
-        return ResponseEntity.ok(newBlogAfterUpdate);
+    public ResponseEntity<?> changeBlogById(@PathVariable String id, @RequestBody Blog blog) {
+        try{
+            Blog newBlogAfterUpdate = blogService.changeBlogById(id, blog);
+            return ResponseEntity.ok(newBlogAfterUpdate);
+        }catch(Exception err){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrrorException(500, "Internal Server Error"));
+        }
     }
 
 }
